2a6ed6fbda03bcc0404e7bdb4785e9810661bcfc
Rene <renesuar99@gmail.com>
2022-06-01 20:25:20 -0400

    Finished first four exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+let word = ""; 
+let num = 0;
+let string = "";
 
+const repeatString = function (word, num) {
+for (let i=0; i < num ; i++){
+  
+   string += word;
+
+}
+return string;
 };
 
 // Do not edit below this line
